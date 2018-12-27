9469bb1770c70a2c74969fc18e5b982d22cca143
Jared Vargason <jaredvargason@gmail.com>
2018-12-26 21:50:18 -0500

    gitignore fix

--- settings.py
+++
@@ -1,7 +0,0 @@
-'''settings.py'''
-HOST = 'irc.chat.twitch.tv'
-PORT = 6667
-USERNAME = 'shiggles_bot'
-CHANNEL = 'gitsandshiggles'
-CLIENT_ID = 'd9kxoi3w8xg75uj6ml2d686tpubmcg'
-OAUTH_TOKEN = 'oauth:6kzt6aw0bmzi4f2khn4zotk77si2ef' 
